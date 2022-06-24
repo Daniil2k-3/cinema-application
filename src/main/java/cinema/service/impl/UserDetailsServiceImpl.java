44a9afc99608291a0b5999e8d2022a51829980d0
DanyloTiazhev <finckreg@gmail.com>
2022-06-24 12:09:35 +0300

    cinema logic implemented with spring

---
+++ src/main/java/cinema/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package cinema.service.impl;
+
+import cinema.model.User;
+import cinema.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userByEmail = userService.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException("Can't find user by email"));
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User
+                        .withUsername(userByEmail.getEmail());
+        builder.password(userByEmail.getPassword());
+        builder.authorities(userByEmail.getRoles()
+                .stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
